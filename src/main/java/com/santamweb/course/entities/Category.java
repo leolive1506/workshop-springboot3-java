a4c7d7692470ad943b9b3870788300210e4856fc
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-18 13:56:30 -0300

    many to many association with JoinTable

--- src/main/java/com/santamweb/course/entities/Category.java
+++ src/main/java/com/santamweb/course/entities/Category.java
@@ -4,18 +4,22 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
   private static final long serialVersionUID = 1L;
-  @Transient
+
+  @JsonIgnore
+  @ManyToMany(mappedBy = "categories")
   private Set<Product> products = new HashSet<>();
 
   @Id
