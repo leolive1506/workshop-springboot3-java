e4da04a41c64a04e966d22d28a156ea890be00de
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-19 21:30:11 -0300

    exception handler delete

--- src/main/java/com/santamweb/course/services/UserService.java
+++ src/main/java/com/santamweb/course/services/UserService.java
@@ -3,10 +3,14 @@ package com.santamweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import com.santamweb.course.entities.User;
 import com.santamweb.course.repositories.UserRepository;
+import com.santamweb.course.services.exceptions.DatabaseException;
 import com.santamweb.course.services.exceptions.ResourceNotFoundException;
 
 // registra como componente do spring
@@ -29,7 +33,13 @@ public class UserService {
   }
 
   public void delete(Long id) {
-    repository.deleteById(id);
+    try {
+      repository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException(e.getMessage());
+    }
   }
 
   public User update(Long id, User obj) {
